d12c6a116a5010743be47625097b8ef931a34e3b
qinmingyuan <mingyuan0715@foxmail.com>
2020-12-28 10:31:56 +0800

    moved unused event

--- app/assets/javascript/rails_ui/application.js
+++ app/assets/javascript/rails_ui/application.js
@@ -13,11 +13,6 @@ document.addEventListener('turbo:load', () => {
 document.addEventListener('turbo:visit', () => {
   timeForLocalized()
 })
-document.addEventListener('ajax:beforeSend', event => {
-  let xhr = event.detail[0]
-  xhr.setRequestHeader('Utc-Offset', (new Date).getTimezoneOffset())
-  //xhr.setRequestHeader('X-Csp-Nonce', Rails.cspNonce())
-})
 document.addEventListener('turbo:before-fetch-request', event => {
   let xhr = event.detail.fetchOptions
   xhr.headers['Utc-Offset'] = (new Date).getTimezoneOffset()
