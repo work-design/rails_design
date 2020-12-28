0b6d5fba391b473b97205c40c66f09899bb48101
qinmingyuan <mingyuan0715@foxmail.com>
2020-12-28 16:44:41 +0800

    simple files

--- app/assets/javascript/rails_ui/index.js
+++ app/assets/javascript/rails_ui/index.js
@@ -1,2 +1,10 @@
-import './application'
 import '../stimulus_com'
+
+import * as Turbo from '@hotwired/turbo'
+window.Turbo = Turbo
+
+document.addEventListener('turbo:before-fetch-request', event => {
+  let xhr = event.detail.fetchOptions
+  xhr.headers['Utc-Offset'] = (new Date).getTimezoneOffset()
+  //xhr.headers['X-Csp-Nonce'] = Rails.cspNonce()
+})
