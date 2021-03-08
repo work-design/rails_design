b2532da8ab39975d0548970e4a860c191444e882
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-08 09:48:24 +0800

    simpel

--- app/assets/javascript/stimulus_phone/wxpay.js
+++ app/assets/javascript/stimulus_phone/wxpay.js
@@ -1,6 +1,7 @@
 import { Controller } from 'stimulus'
 
 class WxpayController extends Controller {
+  static values = { params: Object }
 
   connect() {
     console.debug('Wxpay Controller works!')
@@ -10,41 +11,16 @@ class WxpayController extends Controller {
   chooseWXPay() {
     wx.ready(() => {
       wx.chooseWXPay({
-        timestamp: this.timestamp,
-        nonceStr: this.nonceStr,
-        package: this.package,
-        signType: this.signType,
-        paySign: this.paySign,
+        ...this.paramsValue,
         success: (res) => {
           console.log(res)
           this.element.style.removeProperty('display')
-          //Turbo.visit(location.href, { action: 'replace' })
         },
         error: (e) => {
           alert(e)
         }
       })
     })
-  }
-
-  get timestamp() {
-    return this.data.get('timestamp')
-  }
-
-  get nonceStr() {
-    return this.data.get('nonceStr')
-  }
-
-  get package() {
-    return this.data.get('package')
-  }
-
-  get signType() {
-    return this.data.get('signType')
-  }
-
-  get paySign() {
-    return this.data.get('paySign')
   }
 
 }
