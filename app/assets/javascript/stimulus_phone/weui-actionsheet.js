6de6f386c1faebf63a3621a40265d86323b58ecb
qinmingyuan <mingyuan0715@foxmail.com>
2021-04-06 00:20:00 +0800

    support actionsheetp

---
+++ app/assets/javascript/stimulus_phone/weui-actionsheet.js
@@ -0,0 +1,45 @@
+import { Controller } from 'stimulus'
+
+class WeuiActionsheetController extends Controller {
+  static targets = ['mask', 'sheet']
+  static values = {
+    id: String
+  }
+
+  connect() {
+    console.debug(this.identifier, 'connected!')
+  }
+
+  close() {
+    let ele = this.maskTarget
+    ele.style.display = 'none'
+    ele.style.opacity = 0
+    ele.style.transition = 'opacity 2s'
+    this.sheetTarget.classList.remove('weui-actionsheet_toggle')
+  }
+
+  show() {
+    let ele = this.maskTarget
+    ele.style.display = 'block'
+    ele.style.opacity = 1
+    ele.style.transition = 'opacity 2s'
+    this.sheetTarget.classList.add('weui-actionsheet_toggle')
+  }
+
+  toggle() {
+    let x = this.target
+    if (x.sheetTarget.classList.contains('weui-actionsheet_toggle')) {
+      x.close()
+    } else {
+      x.show()
+    }
+  }
+
+  get target() {
+    let ele = document.getElementById(this.idValue)
+    return application.getControllerForElementAndIdentifier(ele, 'weui-actionsheet')
+  }
+
+}
+
+application.register('weui-actionsheet', WeuiActionsheetController)
