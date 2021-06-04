581f9950fc143574c2974f090071c732261525ea
qinmingyuan <mingyuan0715@foxmail.com>
2021-06-04 10:55:39 +0800

    reload logic works

--- app/assets/javascript/stimulus_com/visit.js
+++ app/assets/javascript/stimulus_com/visit.js
@@ -24,7 +24,7 @@ class VisitController extends Controller {
     }
 
     document.documentElement.classList.remove('is-clipped')
-    //this.element.remove()
+    this.element.remove()
   }
 
   addEvent(headers) {
