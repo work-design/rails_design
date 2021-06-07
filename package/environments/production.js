9dc7f83d157027a1a575cf2855a05140d09987d1
qinmingyuan <mingyuan0715@foxmail.com>
2021-06-07 15:48:15 +0800

    moved unused

--- package/environments/production.js
+++ package/environments/production.js
@@ -1,30 +1,12 @@
 /* eslint global-require: 0 */
 /* eslint import/no-dynamic-require: 0 */
 
-const CompressionPlugin = require('compression-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
 const baseConfig = require('./base')
 
 const getPlugins = () => {
   const plugins = []
 
-  plugins.push(
-    new CompressionPlugin({
-      filename: '[path][base].gz[query]',
-      algorithm: 'gzip',
-      test: /\.(js|css|html|json|ico|svg|eot|otf|ttf|map)$/
-    })
-  )
-
-  if ('brotli' in process.versions) {
-    plugins.push(
-      new CompressionPlugin({
-        filename: '[path][base].br[query]',
-        algorithm: 'brotliCompress',
-        test: /\.(js|css|html|json|ico|svg|eot|otf|ttf|map)$/
-      })
-    )
-  }
+  plugins.push()
 
   return plugins
 }
