3406f72c424eda94e75d766ca892cde5eb62d6a3
qinmingyuan <mingyuan0715@foxmail.com>
2021-06-08 14:59:03 +0800

    minifest

--- lib/viter/manifest.rb
+++ lib/viter/manifest.rb
@@ -49,70 +49,57 @@ class Viter::Manifest
     lookup(name, pack_type) || handle_missing_entry(name, pack_type)
   end
 
-  private
-    def compiling?
-      config.compile? && !dev_server.running?
-    end
+  def compiling?
+    config.compile? && !dev_server.running?
+  end
 
-    def compile
-      Webpacker.logger.tagged("Webpacker") { compiler.compile }
-    end
+  def compile
+    Webpacker.logger.tagged("Webpacker") { compiler.compile }
+  end
 
-    def data
-      if config.cache_manifest?
-        @data ||= load
-      else
-        refresh
-      end
+  def data
+    if config.cache_manifest?
+      @data ||= load
+    else
+      refresh
     end
+  end
 
-    def find(name)
-      data[name.to_s].presence
-    end
+  def find(name)
+    data[name.to_s].presence
+  end
 
-    def full_pack_name(name, pack_type)
-      return name unless File.extname(name.to_s).empty?
-      "#{name}.#{manifest_type(pack_type)}"
-    end
+  def full_pack_name(name, pack_type)
+    return name unless File.extname(name.to_s).empty?
+    "#{name}.#{manifest_type(pack_type)}"
+  end
 
-    def handle_missing_entry(name, pack_type)
-      raise Webpacker::Manifest::MissingEntryError, missing_file_from_manifest_error(full_pack_name(name, pack_type[:type]))
-    end
+  def handle_missing_entry(name, pack_type)
+    raise Webpacker::Manifest::MissingEntryError, missing_file_from_manifest_error(full_pack_name(name, pack_type[:type]))
+  end
 
-    def load
-      if config.public_manifest_path.exist?
-        JSON.parse config.public_manifest_path.read
-      else
-        {}
-      end
+  def load
+    if config.public_manifest_path.exist?
+      JSON.parse config.public_manifest_path.read
+    else
+      {}
     end
+  end
 
-    # The `manifest_name` method strips of the file extension of the name, because in the
-    # manifest hash the entrypoints are defined by their pack name without the extension.
-    # When the user provides a name with a file extension, we want to try to strip it off.
-    def manifest_name(name, pack_type)
-      return name if File.extname(name.to_s).empty?
-      File.basename(name, ".#{pack_type}")
-    end
+  # The `manifest_name` method strips of the file extension of the name, because in the
+  # manifest hash the entrypoints are defined by their pack name without the extension.
+  # When the user provides a name with a file extension, we want to try to strip it off.
+  def manifest_name(name, pack_type)
+    return name if File.extname(name.to_s).empty?
+    File.basename(name, ".#{pack_type}")
+  end
 
-    def manifest_type(pack_type)
-      case pack_type
-      when :javascript then 'js'
-      when :stylesheet then 'css'
-      else pack_type.to_s
-      end
+  def manifest_type(pack_type)
+    case pack_type
+    when :javascript then 'js'
+    when :stylesheet then 'css'
+    else pack_type.to_s
     end
+  end
 
-    def missing_file_from_manifest_error(bundle_name)
-      <<-MSG
-Webpacker can't find #{bundle_name} in #{config.public_manifest_path}. Possible causes:
-1. You want to set viter.yml value of compile to true for your environment
-   unless you are using the `webpack -w` or the webpack-dev-server.
-2. webpack has not yet re-run to reflect updates.
-3. You have misconfigured Webpacker's config/viter.yml file.
-4. Your webpack configuration is not creating a manifest.
-Your manifest contains:
-#{JSON.pretty_generate(@data)}
-      MSG
-    end
 end
