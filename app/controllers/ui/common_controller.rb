cd96db43d95bf1b06e014efc630343d707abf8a2
qinmingyuan <mingyuan0715@foxmail.com>
2021-06-09 23:40:48 +0800

    send file directly

--- app/controllers/ui/common_controller.rb
+++ app/controllers/ui/common_controller.rb
@@ -2,7 +2,19 @@ module Ui
   class CommonController < ActionController::Base
 
     def index
-      path = Pathname.new "/#{params['path']}.#{request.format.symbol}"
+      path = Pathname.new "/#{params[:path]}.#{params[:format]}"
+
+      if path.exist? && Rails.env.development?
+        send_file path
+      else
+        header :ok
+      end
+    end
+
+    def image
+      file = "#{params[:path]}.#{params[:format]}"
+      path = Rails.root.join('app/packs/images', file)
+
       if path.exist? && Rails.env.development?
         send_file path
       else
