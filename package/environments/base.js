65f3159cdf8a9ec0d7e1e9a47a64e731db1acf08
qinmingyuan <mingyuan0715@foxmail.com>
2021-06-10 15:29:22 +0800

    moved repeat config

--- package/environments/base.js
+++ package/environments/base.js
@@ -1,45 +1,10 @@
 /* eslint global-require: 0 */
 /* eslint import/no-dynamic-require: 0 */
 
-const { basename, dirname, join, relative, resolve } = require('path')
-const { sync: globSync } = require('glob')
-const extname = require('path-complete-extname')
+const { join } = require('path')
 const config = require('../config')
 const rootPath = join(config.root_path)
-
-const getEntryObject = (rootPath) => {
-  const entries = {}
-
-  globSync(`${rootPath}/**/*.*`).forEach((path) => {
-    const namespace = relative(join(rootPath), dirname(path))
-    const name = join(namespace, basename(path, extname(path)))
-    let assetPaths = resolve(path)
-
-    let previousPaths = entries[name]
-    if (previousPaths) {
-      previousPaths = Array.isArray(previousPaths)
-        ? previousPaths
-        : [previousPaths]
-      previousPaths.push(assetPaths)
-      assetPaths = previousPaths
-    }
-
-    entries[name] = assetPaths
-  })
-
-  return entries
-}
-
-const getModulePaths = () => {
-  const result = [resolve(config.source_path)]
-
-  if (config.additional_paths) {
-    config.additional_paths.forEach((path) => result.push(resolve(path)))
-  }
-  result.push('node_modules')
-
-  return result
-}
+const getEntryObject = require('../paths')
 
 module.exports = {
   root: join(process.cwd(), config.root_path),
@@ -52,7 +17,7 @@ module.exports = {
   },
   build: {
     rollupOptions: {
-      input: getEntryObject()
+      input: getEntryObject(rootPath)
     }
   }
 }
