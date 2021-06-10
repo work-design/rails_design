6b069b10aa63ea790cccf5a0df93b7a9d36a2292
qinmingyuan <mingyuan0715@foxmail.com>
2021-06-10 16:47:42 +0800

    add more engine paths

--- lib/rails_ui/exporter.rb
+++ lib/rails_ui/exporter.rb
@@ -8,7 +8,7 @@ module RailsUi
       vite = YamlHelper.new(template: 'config/viter_template.yml', export: 'config/viter.yml')
 
       Rails::Engine.subclasses.each do |engine|
-        asset_root = engine.root.join('app/assets', 'entrypoints')
+        asset_root = engine.root.join('app/assets')
         if asset_root.directory?
           vite.add 'alias', { "#{engine.engine_name}_ui" => asset_root.to_s }
         end
@@ -17,6 +17,11 @@ module RailsUi
         if view_root.directory?
           vite.append 'engine_paths', view_root.to_s
           vite.add 'alias', { "#{engine.engine_name}_view" => view_root.to_s }
+        end
+
+        entrypoint_root = engine.root.join('app/assets', 'entrypoints')
+        if entrypoint_root.directory?
+          vite.append 'engine_paths', entrypoint_root
         end
       end
 
