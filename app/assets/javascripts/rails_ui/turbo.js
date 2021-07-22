d592e8d05e6637a98728bc675d08d5ba81d6fd9e
qinmingyuan <mingyuan0715@foxmail.com>
2021-07-22 23:46:21 +0800

    comment

--- app/assets/javascripts/rails_ui/turbo.js
+++ app/assets/javascripts/rails_ui/turbo.js
@@ -14,6 +14,7 @@ document.addEventListener('turbo:before-cache', event => {
   }
 })
 
+// 当 target 为 body 的时候，则不用 getElementById 的逻辑，而是直接使用body
 Object.defineProperties(customElements.get('turbo-stream').prototype, {
   targetElement: {
     get: function() {
