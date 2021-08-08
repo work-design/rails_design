fa662d7a04a9e076d61a11134517313ad42a70f5
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-09 00:12:33 +0800

    instance an app;

--- app/assets/javascripts/stimulus_com/vue.js
+++ app/assets/javascripts/stimulus_com/vue.js
@@ -3,19 +3,13 @@ import { createApp } from 'vue'
 
 class VueController extends Controller {
 
-  reload(element) {
-  }
-
   connect() {
     console.debug('connected:', this.identifier)
     this.app.mount(this.element)
   }
 
   get app() {
-    const app = createApp({})
-    app.config.performance = true
-    window.app = app
-    return app
+    return createApp({})
   }
 
 }
