f68b95b728670755810976505619e331087b734b
qinmingyuan <mingyuan0715@foxmail.com>
2022-02-13 18:18:02 +0800

    agent config

--- app/assets/javascripts/stimulus_qy/config.js
+++ app/assets/javascripts/stimulus_qy/config.js
@@ -2,12 +2,24 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static values = {
-    params: Object
+    options: Object,
+    debug: Boolean,
+    apis: Array
   }
-  static targets = ['load']
 
   connect() {
-    wx.agentConfig()
+    const options = this.optionsValue
+    wx.agentConfig({
+      corpid: options['corpid'],
+      agentid: options['agentid'],
+      timestamp: options['timestamp'],
+      nonceStr: options['noncestr'],
+      signature: options['signature'],
+      jsApiList: this.apisValue,
+      success: function(res) {
+        console.debug('res', res)
+      }
+    })
   }
 
 }
