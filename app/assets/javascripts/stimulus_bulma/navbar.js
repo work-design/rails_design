bfd9251870cb4a97091d11432766ab2add86ba8a
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-28 21:08:19 +0800

    stimulus

--- app/assets/javascripts/stimulus_bulma/navbar.js
+++ app/assets/javascripts/stimulus_bulma/navbar.js
@@ -1,7 +1,28 @@
 import { Controller } from '@hotwired/stimulus'
+import { application } from '../rails_design/stimulus'
 
 export default class extends Controller {
   static targets = ['menu']
+  static classes = ['look']
+  static values = {
+    scroll: Boolean
+  }
+
+  connect() {
+    this.scrollLook()
+  }
+
+  scrollLook() {
+    if (this.scrollValue) {
+      document.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 0) {
+          this.element.classList.add('is-dark')
+        } else {
+          this.element.classList.remove('is-dark')
+        }
+      })
+    }
+  }
 
   toggle(element) {
     element.currentTarget.classList.toggle('is-active')
