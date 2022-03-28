f54a5f2600d8a596dc7f0aa9d86f7717f6843652
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-28 12:57:02 +0800

    moved unused code

--- app/assets/javascripts/stimulus_com/show.js
+++ app/assets/javascripts/stimulus_com/show.js
@@ -3,21 +3,13 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['src', 'item']
 
-  initEvent() {
-    let ele = this.element
-    ele.addEventListener('mouseenter', this.showItem)
-    ele.addEventListener('mouseleave', this.hideItem)
-  }
-
   show() {
-    //console.log(this)
     this.itemTargets.forEach(el => {
       el.style.visibility = 'visible'
     })
   }
 
-  hide(event) {
-    //console.log(event.target)
+  hide() {
     this.itemTargets.forEach(el => {
       el.style.visibility = 'hidden'
     })
