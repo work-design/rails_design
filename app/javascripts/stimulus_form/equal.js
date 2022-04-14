b9975aa5e902577445b8b7a225c90dba4d806ba1
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-15 00:33:16 +0800

    gemf

--- app/javascripts/stimulus_form/equal.js
+++ app/javascripts/stimulus_form/equal.js
@@ -1,13 +1,25 @@
 import { Controller } from '@hotwired/stimulus'
+import { DateTime } from 'luxon'
 
 export default class extends Controller {
   static targets = ['goal']
+  static values = {
+    plus: Object
+  }
 
   update(event) {
     const ele = event.currentTarget
 
     if (parseFloat(ele.value) > parseFloat(this.goalTarget.value)) {
       this.goalTarget.value = ele.value
+    }
+  }
+
+  updateTime(event) {
+    const ele = event.currentTarget
+    const dt = DateTime.fromISO(ele.value).plus(this.plusValue).toFormat("yyyy-MM-dd'T'HH:mm:ss")
+    if (this.goalTarget.value === this.goalTarget.defaultValue) {
+      this.goalTarget.value = dt
     }
   }
 
