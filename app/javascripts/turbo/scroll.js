1db8ac1f787bbff0ba837186098b69d6da74935c
qinmingyuan <mingyuan0715@foxmail.com>
2022-04-19 12:04:44 +0800

    use sssionstorage to store scrolltop

--- app/javascripts/turbo/scroll.js
+++ app/javascripts/turbo/scroll.js
@@ -1,24 +1,20 @@
-let scrollTop = 0
 window.addEventListener('turbo:click', event => {
-  console.debug('turbo:click', event)
   if (event.target.hasAttribute('data-turbo-scroll')) {
-    scrollTop = document.scrollingElement.scrollTop
+    sessionStorage.setItem('scrollTop', document.scrollingElement.scrollTop)
   }
 })
 
 window.addEventListener('turbo:submit-start', event => {
-  console.debug('turbo:submit-start', event)
   if (event.target.hasAttribute('data-turbo-scroll')) {
-    scrollTop = document.scrollingElement.scrollTop
+    sessionStorage.setItem('scrollTop', document.scrollingElement.scrollTop)
   }
 })
 
 window.addEventListener('turbo:render', event => {
-  if (scrollTop) {
-    document.scrollingElement.scrollTo(0, scrollTop)
+  if (sessionStorage.getItem('scrollTop')) {
+    document.scrollingElement.scrollTo(0, sessionStorage.getItem('scrollTop'))
     Turbo.navigator.currentVisit.scrolled = true
   }
 
-  scrollTop = 0
-  console.debug('turbo:render', event)
+  sessionStorage.removeItem('scrollTop')
 })
