932e37b5435029bcb70fe64c1f7d5eb2e50b271c
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-20 20:32:19 +0800

    print

--- app/javascripts/stimulus_com/print.js
+++ app/javascripts/stimulus_com/print.js
@@ -7,17 +7,22 @@ export default class extends Controller {
   }
 
   doPrint() {
-    const iframe = document.createElement('iframe')
-
-    iframe.src = this.urlValue
-    iframe.name = 'pdf'
-    iframe.hidden = true
-    iframe.onload = () => {
-      const pdfFrame = window.frames['pdf']
-      pdfFrame.focus()
-      pdfFrame.print()
+    const present_frame = document.getElementById('pdf_iframe')
+    if (present_frame) {
+      present_frame.src = this.urlValue
+    } else {
+      const iframe = document.createElement('iframe')
+      iframe.src = this.urlValue
+      iframe.name = 'pdf'
+      iframe.id = 'pdf_iframe'
+      iframe.hidden = true
+      iframe.onload = () => {
+        const pdfFrame = window.frames['pdf']
+        pdfFrame.focus()
+        pdfFrame.print()
+      }
+      document.body.appendChild(iframe)
     }
-    document.body.appendChild(iframe)
   }
 
 }
