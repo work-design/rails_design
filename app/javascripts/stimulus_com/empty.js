6ca03d11bb8d012f2ac0064402912604bcec4df5
qinmingyuan <mingyuan0715@foxmail.com>
2022-08-01 10:12:27 +0800

    empty logic

--- app/javascripts/stimulus_com/empty.js
+++ app/javascripts/stimulus_com/empty.js
@@ -1,10 +1,12 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['content']
+  static targets = ['content', 'text']
 
   connect() {
     if (this.hasContentTarget && this.contentTarget.childElementCount < 1) {
+      this.element.remove()
+    } else if (this.hasTextTarget && this.textTarget.innerText.length < 1) {
       this.element.remove()
     } else if (this.element.childElementCount < 1) {
       this.element.remove()
