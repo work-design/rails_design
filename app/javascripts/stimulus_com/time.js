323c9802a0e7ac7d26e4936167b7463d5652df3e
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-14 15:47:24 +0800

    make time format more better

--- app/javascripts/stimulus_com/time.js
+++ app/javascripts/stimulus_com/time.js
@@ -5,7 +5,8 @@ window.DateTime = DateTime
 // data-controller="time"
 export default class extends Controller {
   static values = {
-    localized: Boolean
+    localized: Boolean,
+    datetime: String
   }
 
   connect() {
@@ -13,12 +14,22 @@ export default class extends Controller {
   }
 
   parse() {
-    if (this.element.innerText.length > 0 && this.localizedValue !== true) {
-      const format_str = this.element.dataset['format'] || 'yyyy-MM-dd HH:mm'
-      const time = DateTime.fromISO(this.element.innerText)
-      this.element.innerText = time.toFormat(format_str)
-      this.localizedValue = true
+    if (this.localizedValue) {
+      return
     }
+    let str
+    if (this.hasDatetimeValue && this.datetimeValue) {
+      str = this.datetimeValue
+    } else if (this.element.innerText.length > 0) {
+      str = this.element.innerText
+    } else {
+      return
+    }
+
+    const format_str = this.element.dataset['format'] || 'yyyy-MM-dd HH:mm'
+    const time = DateTime.fromISO(str)
+    this.element.innerText = time.toFormat(format_str)
+    this.localizedValue = true
   }
 
 }
