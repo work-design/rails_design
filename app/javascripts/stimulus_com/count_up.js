94fd2d36fd5963fe6b273b0af94f8312f014a702
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-14 15:50:00 +0800

    icon

--- app/javascripts/stimulus_com/count_up.js
+++ app/javascripts/stimulus_com/count_up.js
@@ -14,7 +14,7 @@ export default class extends Controller {
   countUp() {
     const time = DateTime.fromISO(this.timeValue)
     let result = DateTime.now().diff(time, ['days', 'hours', 'minutes', 'seconds'])
-    let format = ['d天', 'h时', 'mm分', 'ss秒']
+    let format = ['d天', 'h时', 'm分', 's秒']
     let result_format
 
     const timer = setInterval(() => {
