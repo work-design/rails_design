391ce9e2b7018b83c821c8eb8844b3a309f47f4f
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-21 10:47:44 +0800

    use load event

--- app/javascripts/stimulus_phone/config.js
+++ app/javascripts/stimulus_phone/config.js
@@ -8,38 +8,37 @@ export default class extends Controller {
   }
 
   initialize() {
-    this.script = document.createElement('script')
-    this.script.setAttribute('src', 'https://res.wx.qq.com/open/js/jweixin-1.6.0.js')
-    this.script.setAttribute('referrerpolicy', 'origin')
+
   }
 
   connect() {
-    const options = this.optionsValue
-
-    wx.config({
-      debug: this.debugValue,
-      appId: options['appid'],
-      timestamp: options['timestamp'],
-      nonceStr: options['noncestr'],
-      signature: options['signature'],
-      jsApiList: this.apisValue,
-      openTagList: ['wx-open-subscribe']
-    })
-    wx.ready(() => {
-      console.debug('ready, ok')
-    })
-    wx.error(res => {
-      if (this.debugValue) {
-        alert('wx.config: ' + JSON.stringify(res) + '\n' + `location: ${location.href}`)
-      } else {
-        console.debug('wx.config:', res)
-      }
+    this.script.addEventListener('load', (event) => {
+      window.xxx = event
+      const options = this.optionsValue
+      wx.config({
+        debug: this.debugValue,
+        appId: options['appid'],
+        timestamp: options['timestamp'],
+        nonceStr: options['noncestr'],
+        signature: options['signature'],
+        jsApiList: this.apisValue,
+        openTagList: ['wx-open-subscribe']
+      })
+      wx.ready(() => {
+        console.debug('ready, ok')
+      })
+      wx.error(res => {
+        if (this.debugValue) {
+          alert('wx.config: ' + JSON.stringify(res) + '\n' + `location: ${location.href}`)
+        } else {
+          console.debug('wx.config:', res)
+        }
+      })
     })
   }
 
-  disconnect() {
-    //this.script.remove()
-    // window.wx = undefined  todo should implement better
+  get script() {
+    return document.getElementById('weixin_script')
   }
 
 }
