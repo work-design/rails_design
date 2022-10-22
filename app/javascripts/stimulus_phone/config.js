29ad5f03b05d957a4435b26e6841e11ade93faeb
qinmingyuan <mingyuan0715@foxmail.com>
2022-10-22 10:51:07 +0800

    config js use fetch

--- app/javascripts/stimulus_phone/config.js
+++ app/javascripts/stimulus_phone/config.js
@@ -1,38 +1,40 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static values = {
-    options: Object,
-    debug: Boolean,
-    apis: { type: Array, default: [] }
-  }
 
   initialize() {
-
-  }
-
-  connect() {
-    this.script.addEventListener('load', (event) => {
-      window.xxx = event
-      const options = this.optionsValue
-      wx.config({
-        debug: this.debugValue,
-        appId: options['appid'],
-        timestamp: options['timestamp'],
-        nonceStr: options['noncestr'],
-        signature: options['signature'],
-        jsApiList: this.apisValue,
-        openTagList: ['wx-open-subscribe']
-      })
-      wx.ready(() => {
-        console.debug('ready, ok')
-      })
-      wx.error(res => {
-        if (this.debugValue) {
-          alert('wx.config: ' + JSON.stringify(res) + '\n' + `location: ${location.href}`)
-        } else {
-          console.debug('wx.config:', res)
-        }
+    this.script.addEventListener('load', () => {
+      fetch('/wechat/js', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          url: location.href
+        })
+      }).then(response => {
+        return response.json()
+      }).then(body => {
+        wx.config({
+          debug: body['debug'],
+          appId: body['appid'],
+          timestamp: body['timestamp'],
+          nonceStr: body['noncestr'],
+          signature: body['signature'],
+          jsApiList: body['apis'],
+          openTagList: ['wx-open-subscribe']
+        })
+        wx.ready(() => {
+          console.debug('ready, ok')
+        })
+        wx.error(res => {
+          if (body['debug']) {
+            alert('wx.config: ' + JSON.stringify(res) + '\n' + `location: ${location.href}`)
+          } else {
+            console.debug('wx.config:', res)
+          }
+        })
       })
     })
   }
