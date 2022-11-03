81a8f2c6ee51debeca61aec837f954e7db338417
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-03 10:46:05 +0800

    wechat js support url

--- app/javascripts/rails_design/wechat_js.js
+++ app/javascripts/rails_design/wechat_js.js
@@ -1,39 +1,44 @@
 const weixin_script = document.getElementById('weixin_script')
-
+const weixin_fetch = function(url = location.href, appid) {
+  console.debug('=====', url, appid)
+  fetch('/wechat/js', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      url: url,
+      appid: appid
+    })
+  }).then(response => {
+    return response.json()
+  }).then(body => {
+    wx.config({
+      debug: body['debug'],
+      appId: body['appid'],
+      timestamp: body['timestamp'],
+      nonceStr: body['noncestr'],
+      signature: body['signature'],
+      jsApiList: body['apis'],
+      openTagList: ['wx-open-subscribe']
+    })
+    wx.ready(() => {
+      console.debug('ready, ok')
+    })
+    wx.error(res => {
+      if (body['debug']) {
+        alert('wx.config: ' + JSON.stringify(res) + '\n' + `location: ${location.href}`)
+      } else {
+        console.debug('wx.config:', res)
+      }
+    })
+  })
+}
+window.weixin_fetch = weixin_fetch
 if (weixin_script) {
   weixin_script.addEventListener('load', (event) => {
     console.debug('weixin script load trigger', event)
-    fetch('/wechat/js', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        url: location.href
-      })
-    }).then(response => {
-      return response.json()
-    }).then(body => {
-      wx.config({
-        debug: body['debug'],
-        appId: body['appid'],
-        timestamp: body['timestamp'],
-        nonceStr: body['noncestr'],
-        signature: body['signature'],
-        jsApiList: body['apis'],
-        openTagList: ['wx-open-subscribe']
-      })
-      wx.ready(() => {
-        console.debug('ready, ok')
-      })
-      wx.error(res => {
-        if (body['debug']) {
-          alert('wx.config: ' + JSON.stringify(res) + '\n' + `location: ${location.href}`)
-        } else {
-          console.debug('wx.config:', res)
-        }
-      })
-    })
+    weixin_fetch()
   })
 }
