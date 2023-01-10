d5676ec317439a9b57498c95946dfee0ba1592de
qinmingyuan <mingyuan0715@foxmail.com>
2023-01-10 18:16:06 +0800

    key boarder js

--- app/javascripts/stimulus_form/keyboard.js
+++ app/javascripts/stimulus_form/keyboard.js
@@ -1,11 +1,23 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static outlets = ['input']
+  static values = {
+    id: String
+  }
 
   enter(event) {
     const element = event.currentTarget
 
-    this.inputOutlet.value = element.innerText
+    this.input.value = this.input.value.concat(element.innerText)
+  }
+
+  backspace(event) {
+    const element = event.currentTarget
+
+    this.input.value = this.input.value.slice(0, -1)
+  }
+
+  get input() {
+    return document.getElementById(this.idValue)
   }
 }
