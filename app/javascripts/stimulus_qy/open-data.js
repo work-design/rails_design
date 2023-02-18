e8afeab30f4106109e56c4fb140463344023c0a1
qinmingyuan <mingyuan0715@foxmail.com>
2023-02-18 21:36:42 +0800

    show error

--- app/javascripts/stimulus_qy/open-data.js
+++ app/javascripts/stimulus_qy/open-data.js
@@ -3,19 +3,26 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
 
   connect() {
-    if (typeof(WWOpenData) === 'object' && WWOpenData.checkSession) {
-      WWOpenData.checkSession({
-        success: () => {
-          const x = this.element.querySelector('ww-open-data')
-          WWOpenData.bind(x)
-          console.debug('有登录态')
-          alert(`open data success ${x.getAttribute('openid')}`)
-        },
-        fail: () => {
-          alert('登录态过期')
-        }
-      })
-      alert('connected')
+    if (typeof(WWOpenData) === 'object') {
+      if (WWOpenData.on) {
+        WWOpenData.on({
+          error: (event) => {
+            alert(`error ${JSON.stringify(event)}`)
+          }
+        })
+      }
+      if (WWOpenData.checkSession) {
+        WWOpenData.checkSession({
+          success: () => {
+            const x = this.element.querySelector('ww-open-data')
+            WWOpenData.bind(x)
+            alert(`open data success ${x.getAttribute('openid')}`)
+          },
+          fail: () => {
+            alert('登录态过期')
+          }
+        })
+      }
     } else {
       alert('WWOpenData fail')
       wxwork_fetch()
