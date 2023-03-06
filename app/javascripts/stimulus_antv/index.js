5a8a272306e8c1aa8c20002c740fac4fadd38440
qinmingyuan <mingyuan0715@foxmail.com>
2023-03-06 13:11:11 +0800

    stimulus base

--- app/javascripts/stimulus_antv/index.js
+++ app/javascripts/stimulus_antv/index.js
@@ -1,4 +1,4 @@
-import { application } from '../rails_design/stimulus'
+import { application } from '../stimulus_base'
 
 import AntvLineController from './antv-line'
 application.register('antv-line', AntvLineController)
