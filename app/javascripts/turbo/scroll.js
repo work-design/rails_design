a966dc591543d4dd13ec348c6782d7d8d99a5da5
qinmingyuan <mingyuan0715@foxmail.com>
2023-04-06 17:36:27 +0800

    simple scoll logic

--- app/javascripts/turbo/scroll.js
+++ app/javascripts/turbo/scroll.js
@@ -1,36 +1,33 @@
-window.addEventListener('turbo:click', event => {
+window.turboScroll = function(event) {
   if (event.target.dataset.turboScroll === 'true') {
-    sessionStorage.setItem('scrollTop', document.scrollingElement.scrollTop)
-  } else if (!isNaN(event.target.dataset.turboScroll)) {
-    sessionStorage.setItem('scrollTop', event.target.dataset.turboScroll)
+    let ele = event.target.parentElement
+    while (ele) {
+      if (ele.scrollTop > 0) {
+        sessionStorage.setItem('scrollTop', ele.scrollTop)
+        sessionStorage.setItem('scrollContainer', ele.id)
+        break
+      }
+      ele = ele.parentElement
+    }
   }
+}
+
+window.addEventListener('turbo:click', event => {
+  turboScroll(event)
 })
 
 window.addEventListener('turbo:submit-start', event => {
-  if (event.target.dataset.turboScroll === 'true') {
-    if (event.target.dataset.turboScrollContainer) {
-      sessionStorage.setItem('scrollContainer', event.target.dataset.turboScrollContainer)
-      const ele = document.getElementById(event.target.dataset.turboScrollContainer)
-      sessionStorage.setItem('scrollTop', ele.scrollTop)
-    } else {
-      sessionStorage.setItem('scrollTop', document.scrollingElement.scrollTop)
-    }
-  } else if (!isNaN(event.target.dataset.turboScroll)) {
-    sessionStorage.setItem('scrollTop', event.target.dataset.turboScroll)
-  }
+  turboScroll(event)
 })
 
 window.addEventListener('turbo:render', event => {
   if (sessionStorage.getItem('scrollTop')) {
-    if (sessionStorage.getItem('scrollContainer')) {
-      const ele = document.getElementById(sessionStorage.getItem('scrollContainer'))
-      ele.scrollTo(0, sessionStorage.getItem('scrollTop'))
-      sessionStorage.removeItem('scrollContainer')
-    } else {
-      document.scrollingElement.scrollTo(0, sessionStorage.getItem('scrollTop'))
-    }
+    const ele = document.getElementById(sessionStorage.getItem('scrollContainer')) || document.scrollingElement
+    ele.scrollTo(0, sessionStorage.getItem('scrollTop'))
 
     Turbo.navigator.currentVisit.scrolled = true
+
     sessionStorage.removeItem('scrollTop')
+    sessionStorage.removeItem('scrollContainer')
   }
 })
