a1ba4a1154c92911e76256db5a9893239ee6c1ad
qinmingyuan <mingyuan0715@foxmail.com>
2023-10-31 19:32:35 +0800

    auto hidd alter

--- app/javascripts/stimulus_com/notice.js
+++ app/javascripts/stimulus_com/notice.js
@@ -2,9 +2,20 @@ import { Controller } from '@hotwired/stimulus'
 
 // data-controller="notice"
 export default class extends Controller {
+  static values = {
+    duration: { type: Number, default: 5 }
+  }
+
+  connect() {
+    this.xx()
+  }
 
   close() {
     this.element.remove()
+  }
+
+  xx() {
+    this.element.style.animationDuration = `${this.durationValue}s`
   }
 
 }
