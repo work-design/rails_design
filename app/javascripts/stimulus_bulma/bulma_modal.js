d288582be83dfb212335670384933376d2b523b5
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-16 10:34:27 +0800

    simple logic

--- app/javascripts/stimulus_bulma/bulma_modal.js
+++ app/javascripts/stimulus_bulma/bulma_modal.js
@@ -21,9 +21,14 @@ export default class extends Controller {
 
   // 关闭前检查下有没有未提交的表单
   closeCheck() {
-    return Array.from(this.element.getElementsByTagName('input')).reduce((str, el) => {
-      return (el.value !== el.defaultValue) ? str.concat(', ', el.labels[0]?.innerText) : str
-    }, '')
+    let arr = []
+    Array.from(this.element.getElementsByTagName('input')).forEach(el => {
+      if (el.value !== el.defaultValue && el.labels[0]) {
+        arr.push(el.labels[0].innerText)
+      }
+    })
+
+    return arr.join(',')
   }
 
 }
