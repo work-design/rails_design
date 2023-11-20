6d5404a98e679b9ce8251e0b134feb4b2305c9b8
qinmingyuan <mingyuan0715@foxmail.com>
2023-11-20 14:14:23 +0800

    fixed bottom

--- app/javascripts/turbo/events.js
+++ app/javascripts/turbo/events.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbo:before-fetch-request', event => {
   const xhr = event.detail.fetchOptions
-  xhr.headers['Utc-Offset'] = (new Date).getTimezoneOffset()
+  xhr.headers['Timezone'] = Intl.DateTimeFormat().resolvedOptions().timeZone
   //xhr.headers['X-Csp-Nonce'] = Rails.cspNonce()
 })
 
