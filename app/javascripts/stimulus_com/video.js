0ec2a4a1a92d222446b353a6296f03c700a6ae99
qinmingyuan <mingyuan0715@foxmail.com>
2024-01-11 00:06:55 +0800

    the logic remove

--- app/javascripts/stimulus_com/video.js
+++ app/javascripts/stimulus_com/video.js
@@ -1,30 +1,19 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['video']
-
-  connect() {
-    if (this.video.autoplay) {
-      this.video.addEventListener('ended', this.enableLink)
-    }
-  }
 
   disconnect() {
-    this.element.querySelectorAll('audio, video').forEach(el => el.remove())
+    if (['VIDEO', 'AUDIO'].includes(this.element.tagName)) {
+      this.element.remove()
+    } else {
+      this.element.querySelectorAll('audio, video').forEach(el => el.remove())
+    }
   }
 
   enableLink(event) {
     const ele = event.currentTarget.closest('a[disabled=disabled]')
     if (ele) {
       ele.removeAttribute('disabled')
-    }
-  }
-
-  get video() {
-    if (this.hasVideoTarget) {
-      return this.videoTarget
-    } else {
-      return this.element
     }
   }
 
