a1fc1c1a77555f245ee0740c9bae4ca60cf64c1a
qinmingyuan <mingyuan0715@foxmail.com>
2024-02-01 16:30:26 +0800

    index

--- app/javascripts/stimulus_com/gif.js
+++ app/javascripts/stimulus_com/gif.js
@@ -8,28 +8,26 @@ export default class extends Controller {
   connect() {
     if (this.hasDurationValue) {
       setTimeout(() => {
-        this.shownNext(this.element, this.element.nextElementSibling, this.identifier)
+        this.innerShowNext(this.element, this.identifier)
       }, this.durationValue)
     }
   }
 
   showNext(event) {
     const ele = event.currentTarget
+    this.innerShowNext(ele, this.identifier)
+  }
 
+  innerShowNext(ele, identifier) {
     let nextEle = ele.nextElementSibling
     if (ele.dataset.next) {
       nextEle = document.getElementById(ele.dataset.next)
     }
-
     let hiddenEle = ele
     if (ele.dataset.hidden) {
       hiddenEle = document.getElementById(ele.dataset.hidden)
     }
 
-    this.shownNext(hiddenEle, nextEle, this.identifier)
-  }
-
-  shownNext(hiddenEle, nextEle, identifier) {
     hiddenEle.style.display = 'none'
     nextEle.style.removeProperty('display')
     if (nextEle.dataset.controller?.includes('dispatch')) {
