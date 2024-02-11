cb27f9d62e216c7547cbe9b6c0035df8f27bc587
qinmingyuan <mingyuan0715@foxmail.com>
2024-02-11 12:25:39 +0800

    events

--- app/javascripts/turbo/events.js
+++ app/javascripts/turbo/events.js
@@ -3,10 +3,3 @@ document.addEventListener('turbo:before-fetch-request', event => {
   xhr.headers['Timezone'] = Intl.DateTimeFormat().resolvedOptions().timeZone
   //xhr.headers['X-Csp-Nonce'] = Rails.cspNonce()
 })
-
-document.addEventListener('turbo:before-cache', event => {
-  const modal = document.getElementById('modal')
-  if (modal) {
-    modal.removeAttribute('src')
-  }
-})
