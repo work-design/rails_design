b02008dc502b36ee1ae0ca8db32b2edeb62693c5
qinmingyuan <mingyuan0715@foxmail.com>
2024-03-13 09:12:24 +0800

    more simpel method

--- app/javascripts/stimulus_com/scroll.js
+++ app/javascripts/stimulus_com/scroll.js
@@ -3,9 +3,11 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
 
   preserve(event) {
+    let scrollTop
     let oldContainer = event.target.parentElement
     while (oldContainer) {
       if (oldContainer.scrollTop > 0) {
+        scrollTop = oldContainer.scrollTop
         break
       }
       oldContainer = oldContainer.parentElement
@@ -13,8 +15,8 @@ export default class extends Controller {
 
     window.addEventListener('turbo:render', ev => {
       const newContainer = document.getElementById(oldContainer.id) || document.scrollingElement
-      console.debug('old container scroll top', oldContainer.scrollTop)
-      newContainer.scrollTo(0, oldContainer.scrollTop)
+      console.debug('old container scroll top', oldContainer, scrollTop)
+      newContainer.scrollTo(0, scrollTop)
     }, { once: true })
   }
 
