39cdb7ef32dd3a7e8359d50d9ae70db13132607e
qinmingyuan <mingyuan0715@foxmail.com>
2024-03-23 13:22:46 +0800

    more common use

--- app/javascripts/stimulus_form/coeff.js
+++ app/javascripts/stimulus_form/coeff.js
@@ -2,38 +2,35 @@ import { Controller } from '@hotwired/stimulus'
 
 // 根据数量，计算价格
 export default class extends Controller {
-  static targets = ['quantity', 'budget', 'price']
+  static targets = ['quantity', 'total', 'single']
 
-  updatePrice(event) {
-    const price = event.currentTarget
-    let quantity = document.getElementById(price.id.replace('price', 'quantity'))
-    let result = (price.value * quantity.value).toFixed(2)
+  updateSingle(event) {
+    const single = event.currentTarget
+    let quantity = this.quantityTarget
+    let result = (single.value * quantity.value).toFixed(2)
 
-    let budget = document.getElementById(price.id.replace('price', 'budget_amount'))
-    if (budget) {
-      budget.value = result
+    if (this.hasTotalTarget) {
+      this.totalTarget.value = result
     }
   }
 
   updateQuantity(event) {
     let quantity = event.currentTarget
-    let price = document.getElementById(quantity.id.replace('quantity', 'price'))
-    let result = (price.value * quantity.value).toFixed(2)
+    let single = this.singleTarget
+    let result = (single.value * quantity.value).toFixed(2)
 
-    let budget = document.getElementById(quantity.id.replace('quantity', 'budget_amount'))
-    if (budget) {
-      budget.value = result
+    if (this.hasTotalTarget) {
+      this.totalTarget.value = result
     }
   }
 
-  updateBudgetAmount(event) {
-    let budget = event.currentTarget
-    let quantity = document.getElementById(budget.id.replace('budget_amount', 'quantity'))
-    let result = (budget.value / quantity.value).toFixed(2)
+  updateTotal(event) {
+    let total = event.currentTarget
+    let quantity = this.quantityTarget
+    let result = (total.value / quantity.value).toFixed(2)
 
-    let price = document.getElementById(budget.id.replace('budget_amount', 'price'))
-    if (price) {
-      price.value = result
+    if (this.hasSingleTarget) {
+      this.singleTarget.value = result
     }
   }
 
