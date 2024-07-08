ec271a6d1473397022aec3e942c02db1a18c9913
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-08 21:20:50 +0800

    darker

--- app/javascripts/stimulus_form/color-range.js
+++ app/javascripts/stimulus_form/color-range.js
@@ -21,8 +21,11 @@ export default class extends Controller {
   changeAll() {
     [this.hueTarget, this.saturationTarget, this.lightnessTarget].forEach(el => {
       const color = `hsl(${this.hueTarget.value}, ${this.saturationTarget.value}%, ${this.lightnessTarget.value}%)`
+      const darker = (this.lightnessTarget.value * 0.8).toFixed()
+      console.log('-----', darker)
+      const colorDarker = `hsl(${this.hueTarget.value}, ${this.saturationTarget.value}%, ${darker}%)`
       el.setAttribute('style', `background-color: ${color}`)
-      document.getElementById(this.idValue).setAttribute('style', `--admin-menu: ${color}`)
+      document.getElementById(this.idValue).setAttribute('style', `--admin-menu: ${color}; --admin-menu-darker: ${colorDarker}`)
     })
   }
 
