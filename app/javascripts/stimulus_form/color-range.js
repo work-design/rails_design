c1a0eab553e43c581fe5d1fd5065af7bb922d7cf
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-08 21:36:46 +0800

    moved debuginfo

--- app/javascripts/stimulus_form/color-range.js
+++ app/javascripts/stimulus_form/color-range.js
@@ -22,7 +22,6 @@ export default class extends Controller {
     [this.hueTarget, this.saturationTarget, this.lightnessTarget].forEach(el => {
       const color = `hsl(${this.hueTarget.value}, ${this.saturationTarget.value}%, ${this.lightnessTarget.value}%)`
       const darker = (this.lightnessTarget.value * 0.8).toFixed()
-      console.log('-----', darker)
       const colorDarker = `hsl(${this.hueTarget.value}, ${this.saturationTarget.value}%, ${darker}%)`
       el.setAttribute('style', `background-color: ${color}`)
       document.getElementById(this.idValue).setAttribute('style', `--admin-menu: ${color}; --admin-menu-darker: ${colorDarker}`)
