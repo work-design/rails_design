5b5880e1addc4e47da97d1d7a933e287cdc8de0c
qinmingyuan <279378275@qq.com>
2024-08-06 22:12:57 +0800

    add xx

--- app/javascripts/stimulus_com/show.js
+++ app/javascripts/stimulus_com/show.js
@@ -24,7 +24,7 @@ export default class extends Controller {
       this.hiddenTargets.forEach(el => {
         el.classList.add('display-none')
       })
-      document.querySelectorAll(`[name^="${ele.name.replace('xx]', '')}"]`).forEach(ele => {
+      document.querySelectorAll(`[name^="${ele.name.replace('xx]', '')}"]:not(#${ele.id})`).forEach(ele => {
         ele.disabled = false
       })
     } else {
@@ -34,7 +34,7 @@ export default class extends Controller {
       this.hiddenTargets.forEach(el => {
         el.classList.remove('display-none')
       })
-      document.querySelectorAll(`[name^="${ele.name.replace('xx]', '')}"]`).forEach(ele => {
+      document.querySelectorAll(`[name^="${ele.name.replace('xx]', '')}"]:not(#${ele.id})`).forEach(ele => {
         ele.disabled = true
       })
     }
