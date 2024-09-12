f61200baad8ca4f7cca755bf78daf9e1c6a24648
qinmingyuan <279378275@qq.com>
2024-09-12 10:25:00 +0800

    from up method

--- app/javascripts/stimulus_com/time-going.js
+++ app/javascripts/stimulus_com/time-going.js
@@ -10,26 +10,6 @@ export default class extends TimeCountController {
   }
 
   countUp() {
-    const time = DateTime.fromISO(this.timeValue)
-    let result = DateTime.now().diff(time, ['days', 'hours', 'minutes', 'seconds'])
-    let format = ['d天', 'h时', 'mm分', 'ss秒']
-    let result_format
-
-    const timer = setInterval(() => {
-      result = result.plus({ seconds: 1 })
-      for (const [index, value] of Object.values(result.values).entries()) {
-        if (value > 0) {
-          result_format = format.slice(index)
-          break
-        }
-      }
-
-      if (result <= 0) {
-        this.element.textContent = result.toFormat(result_format.join(''))
-        clearInterval(timer)
-      } else {
-        this.element.textContent = result.toFormat(result_format.join(''))
-      }
-    }, 1000, result, this.element)
+    this.count()
   }
 }
