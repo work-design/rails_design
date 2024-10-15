d9f79772c8945cfe6cba1458669ac3da96552d02
qinmingyuan <279378275@qq.com>
2024-10-15 16:02:10 +0800

    suppor auto fade inand out

--- app/javascripts/stimulus_com/clipboard.js
+++ app/javascripts/stimulus_com/clipboard.js
@@ -14,9 +14,12 @@ export default class extends Controller {
   copy() {
     navigator.clipboard?.writeText(this.sourceTarget.textContent)
     if (this.hasToastTarget) {
+      this.toastTarget.style.animationDuration = '2s'
       this.toastTarget.classList.remove('display-none')
+      this.toastTarget.classList.add('has-animate-fade-in-out')
       this.toastTarget.addEventListener('animationend', (event) => {
         event.currentTarget.classList.add('display-none')
+        event.currentTarget.classList.remove('has-animate-fade-in-out')
       }, { once: true })
     }
   }
