865fd0bad456c6919afc1e45acf6401958e5cc13
qinmingyuan <mingyuan0715@foxmail.com>
2025-01-16 00:11:26 +0800

    also add engine

--- lib/rails_design/engine.rb
+++ lib/rails_design/engine.rb
@@ -1,5 +1,9 @@
 module RailsDesign
   class Engine < ::Rails::Engine
 
+    initializer 'rails_design.assets' do |app|
+      app.config.assets.paths << root.join('app/assets/images')
+    end
+
   end
 end
