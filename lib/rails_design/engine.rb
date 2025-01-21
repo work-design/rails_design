d06b3886aea49eeceb8b55fd74ffa913f785542b
qinmingyuan <mingyuan0715@foxmail.com>
2025-01-21 12:21:54 +0800

    add logo

--- lib/rails_design/engine.rb
+++ lib/rails_design/engine.rb
@@ -1,9 +1,5 @@
 module RailsDesign
   class Engine < ::Rails::Engine
 
-    initializer 'rails_design.assets' do |app|
-      app.config.assets.paths << root.join('app/assets/images')
-    end
-
   end
 end
