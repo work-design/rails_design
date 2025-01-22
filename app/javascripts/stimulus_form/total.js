0a5d3795ccf6574d8aaf69031d043b7b7c0847e2
qinmingyuan <mingyuan0715@foxmail.com>
2025-01-23 01:41:10 +0800

    edit price

--- app/javascripts/stimulus_form/total.js
+++ app/javascripts/stimulus_form/total.js
@@ -2,13 +2,25 @@ import { Controller } from '@hotwired/stimulus'
 
 // 根据数量，计算价格
 export default class extends Controller {
-  static targets = ['reduce', 'total']
+  static targets = [
+    'reduce',
+    'profit',
+    'total'
+  ]
 
   updateReduce(event) {
     const reduce = event.currentTarget
 
     if (this.hasTotalTarget) {
-      this.totalTarget.value = (parseFloat(this.totalTarget.defaultValue) + parseFloat(reduce.value) - parseFloat(reduce.defaultValue)).toFixed(2)
+      this.totalTarget.value = (parseFloat(this.totalTarget.defaultValue || 0) + parseFloat(reduce.value) - parseFloat(reduce.defaultValue || 0)).toFixed(2)
+    }
+  }
+
+  updateProfit(event) {
+    const profit = event.currentTarget
+
+    if (this.hasTotalTarget && this.hasReduceTarget) {
+      this.totalTarget.value = (parseFloat(this.reduceTarget.value || 0) + parseFloat(profit.value)).toFixed(2)
     }
   }
 
@@ -16,7 +28,7 @@ export default class extends Controller {
     let total = event.currentTarget
 
     if (this.hasReduceTarget) {
-      this.reduceTarget.value = (parseFloat(this.reduceTarget.defaultValue) + parseFloat(total.value) - parseFloat(total.defaultValue)).toFixed(2)
+      this.reduceTarget.value = (parseFloat(this.reduceTarget.defaultValue || 0) + parseFloat(total.value) - parseFloat(total.defaultValue || 0)).toFixed(2)
     }
   }
 
