651ea4715efa92dffd807aade050d077ae30a72f
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-10 18:22:43 +0800

    clear codes

--- app/javascripts/stimulus_channels/session_channel.js
+++ app/javascripts/stimulus_channels/session_channel.js
@@ -3,26 +3,32 @@ import BaseCable from './base_cable'
 export default class extends BaseCable {
 
   connect() {
-    this.subscription = BaseCable.consumer.subscriptions.create({channel: 'Com::SessionChannel', room: 'room'}, {
-      received(data) {
-        const url = data.url || '/'
-        if (data.auth_token) {
-          document.addEventListener('turbo:before-fetch-request', event => {
-            const xhr = event.detail.fetchOptions
-            xhr.headers['Authorization'] = data.auth_token
-          })
+    this.subscription = BaseCable.consumer.subscriptions.create(
+      {
+        channel: 'Com::SessionChannel',
+        room: 'room'
+      }, 
+      {
+        received(data) {
+          const url = data.url || '/'
+          if (data.auth_token) {
+            document.addEventListener('turbo:before-fetch-request', event => {
+              const xhr = event.detail.fetchOptions
+              xhr.headers['Authorization'] = data.auth_token
+            })
+          }
+          Turbo.visit(url)
+        },
+
+        connected() {
+          console.debug('connected:', this.identifier)
+        },
+
+        disconnected() {
+          console.debug('disconnected:', this.identifier)
         }
-        Turbo.visit(url)
-      },
-
-      connected() {
-        console.debug('connected:', this.identifier)
-      },
-
-      disconnected() {
-        console.debug('disconnected:', this.identifier)
-      }
-    })
+     }
+    )
   }
 
   disconnect() {
