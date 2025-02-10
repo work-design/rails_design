de3bedf5a5a6f14d51837e1fff390101f582c2b3
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-10 22:56:48 +0800

    d

--- app/javascripts/stimulus_channels/log_channel.js
+++ app/javascripts/stimulus_channels/log_channel.js
@@ -3,9 +3,12 @@ import BaseCable from './base_cable'
 export default class extends BaseCable {
 
   connect() {
+    const element = this.element
     this.subscription = BaseCable.consumer.subscriptions.create({ channel: 'Com::LogChannel', room: 'room' }, {
       received(data) {
-        
+        const p = document.createElement('p')
+        p.innerText = data
+        element.appendChild(p)
       }
     })
   }
