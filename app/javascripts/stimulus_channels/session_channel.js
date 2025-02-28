f199d6ff41e7afad531f9147adc0ebd4fb365911
qinmingyuan <mingyuan0715@foxmail.com>
2025-02-28 23:16:35 +0800

    auth

--- app/javascripts/stimulus_channels/session_channel.js
+++ app/javascripts/stimulus_channels/session_channel.js
@@ -11,12 +11,6 @@ export default class extends BaseCable {
       {
         received(data) {
           const url = data.url || '/'
-          if (data.auth_token) {
-            document.addEventListener('turbo:before-fetch-request', event => {
-              const xhr = event.detail.fetchOptions
-              xhr.headers['Authorization'] = data.auth_token
-            })
-          }
           Turbo.visit(url)
         },
 
