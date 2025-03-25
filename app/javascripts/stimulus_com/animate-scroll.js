14371309ff398a35924cad22956c2b3b51ddf6eb
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-25 23:04:19 +0800

    d

--- app/javascripts/stimulus_com/animate-scroll.js
+++ app/javascripts/stimulus_com/animate-scroll.js
@@ -1,27 +1,24 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['src', 'item']
+  static targets = ['scroll']
 
   connect() {
     const ele = this.element
-    ele.classList.remove('invisible')
-    ele.style.animationDuration = `${ele.innerText.length * 200}ms`
-    ele.classList.add('has-animate-typer')
-    ele.addEventListener('animationend', this.typeNext, { once: true })
+    const scroll = this.scrollTarget
+    scroll.style.setProperty('--animate-scroll-to', `translateY(-${scroll.scrollHeight - ele.clientHeight}px)`)
+
+    //ele.classList.remove('invisible')
+    //ele.style.animationDuration = `${ele.innerText.length * 200}ms`
+    //ele.classList.add('has-animate-typer')
+    scroll.addEventListener('animationend', this.scrollBack, { once: true })
   }
 
-  typeNext(event) {
-    const ele = event.currentTarget
-    let nextEle = ele.nextElementSibling
-    while (nextEle) {
-      if (nextEle.innerHTML) {
-        nextEle.dataset.add('controller', 'animate-typer')
-        break
-      } else {
-        nextEle = nextEle.nextElementSibling
-      }
-    }
+  scrollBack(e) {
+    const el = e.currentTarget
+    el.classList.replace('has-animate-scrollUp', 'has-animate-scrollDown')
+    //el.style.setProperty('--animate-scroll-to', `translateY(0)`)
+
   }
 
 }
