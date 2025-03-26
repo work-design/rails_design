f0ff3e393c1b7a14c5b06f91e93366ef8c54db58
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-26 18:11:33 +0800

    animate-scroll

--- app/javascripts/stimulus_com/animate-scroll.js
+++ app/javascripts/stimulus_com/animate-scroll.js
@@ -7,18 +7,6 @@ export default class extends Controller {
     const ele = this.element
     const scroll = this.scrollTarget
     scroll.style.setProperty('--animate-scroll-to', `translateY(-${scroll.scrollHeight - ele.clientHeight}px)`)
-
-    //ele.classList.remove('invisible')
-    //ele.style.animationDuration = `${ele.innerText.length * 200}ms`
-    //ele.classList.add('has-animate-typer')
-   // scroll.addEventListener('animationend', this.scrollBack, { once: true })
-  }
-
-  scrollBack(e) {
-    const el = e.currentTarget
-    el.classList.replace('has-animate-scrollUp', 'has-animate-scrollDown')
-    //el.style.setProperty('--animate-scroll-to', `translateY(0)`)
-
   }
 
 }
