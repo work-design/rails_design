d3e8b40cb8e7b9f2ed516048f494ccaeba11c9a4
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-30 14:39:33 +0800

    ant de

--- app/javascripts/stimulus_antv/index.js
+++ app/javascripts/stimulus_antv/index.js
@@ -1,2 +1,5 @@
 import AntvLineController from './antv-line'
 application.register('antv-line', AntvLineController)
+
+import AntvLineXController from './antv-line-x'
+application.register('antv-line-x', AntvLineXController)
