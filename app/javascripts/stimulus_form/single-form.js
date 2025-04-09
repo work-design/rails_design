4ac2e5d5fd430eb1aa1bd36efe5fe9926d6f5493
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-09 11:44:26 +0800

    moved html 100% logic

--- app/javascripts/stimulus_form/single-form.js
+++ app/javascripts/stimulus_form/single-form.js
@@ -8,10 +8,22 @@ export default class extends BaseController {
     url: String
   }
 
+  connect() {
+    window.addEventListener('resize', () => {
+      // 强制触发重绘
+      document.body.style.display = 'none';
+      document.body.offsetHeight; // 触发重绘
+      document.body.style.display = '';
+    });
+  }
+
   prepare() {
     const clonedItem = this.inputTarget.cloneNode(true)
-    const target = document.getElementById(this.idValue)
-    target.appendChild(clonedItem)
+    this.target.appendChild(clonedItem)
     clonedItem.focus()
+  }
+
+  get target() {
+    return document.getElementById(this.idValue)
   }
 }
