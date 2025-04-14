25c2029f701d9cbd29788b57865c09cf6a668b5e
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-14 17:05:05 +0800

    frame reload

--- app/javascripts/stimulus_com/frame-reload.js
+++ app/javascripts/stimulus_com/frame-reload.js
@@ -3,7 +3,12 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
 
   connect() {
-    this.element.closest('turbo-frame').reload()
+    const frame = this.element.closest('turbo-frame')
+    if (frame && frame.src) {
+      frame.reload()
+    } else {
+      Turbo.visit(location.href, { action: 'replace' })
+    }
   }
 
 }
