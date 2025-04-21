b51e29f59ab2a9e38e250ead7648eb472ff06fc4
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-21 21:55:17 +0800

    d

--- app/javascripts/stimulus_form/single-form.js
+++ app/javascripts/stimulus_form/single-form.js
@@ -20,12 +20,12 @@ export default class extends BaseController {
     const clonedItem = this.inputTarget.cloneNode(true)
     this.target.innerHTML = ''
     this.target.appendChild(clonedItem)
-    this.target.parentNode.classList.remove('display-none')
     visualViewport.addEventListener('resize', () => {
       console.debug('-------------resize', visualViewport.height, this.initHeight)
       if (visualViewport.height < this.initHeight) {
+        //document.documentElement.scrollTo(0, 0)
         this.target.parentNode.style.top = `${visualViewport.height - this.target.parentNode.clientHeight}px`
-        document.documentElement.scrollTo(0, 0)
+        this.target.parentNode.classList.remove('display-none')
       }
     })
     clonedItem.focus()
