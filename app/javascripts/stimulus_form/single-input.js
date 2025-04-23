ffe5ffe710827c2ddd17ac1c9853d600e7e51c29
qinmingyuan <mingyuan0715@foxmail.com>
2025-04-23 22:31:13 +0800

    d

--- app/javascripts/stimulus_form/single-input.js
+++ app/javascripts/stimulus_form/single-input.js
@@ -30,7 +30,7 @@ export default class extends BaseController {
     this.target.type = this.inputTarget.type
     this.target.name = this.inputTarget.name
     this.target.value = this.inputTarget.value
-    this.target.classList.add(ele.tagName.toLowerCase())
+    this.target.classList.add(this.inputTarget.tagName.toLowerCase())
 
     if (visualViewport.height < innerHeight) {
       this.singleFormOutletElement.classList.remove('invisible')
