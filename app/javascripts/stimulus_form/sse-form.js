99274830fea5089e6443ae62192df6aa3dc2a3b1
qinmingyuan <mingyuan0715@foxmail.com>
2025-05-03 20:18:59 +0800

    sse logic

--- app/javascripts/stimulus_form/sse-form.js
+++ app/javascripts/stimulus_form/sse-form.js
@@ -1,8 +1,13 @@
 import { Controller } from '@hotwired/stimulus'
+const EVENT_LINE = 'event:'
+const DATA_LINE = 'data:'
+const ID_LINE = 'id:'
+const RETRY_LINE = 'retry:'
 
 export default class extends Controller {
 
   connect() {
+    this.decoder = new TextDecoder()
   }
 
   commit(event) {
@@ -14,14 +19,36 @@ export default class extends Controller {
       body: new FormData(this.element)
     }).then(async response => {
       const reader = response.body.getReader()
+      let currentBuffer = ''
+      let currentData = ''
+
       while (true) {
         const { done, value } = await reader.read();
-        if (done) break;
-        const chunk = new TextDecoder().decode(value);
-        window.xxx = chunk
-        console.log('Received:', chunk);
-        //container.append(chunk.data.text)
+        if (done) break
+
+        if (typeof value !== 'string') {
+          currentBuffer += this.decoder.decode(value)
+        } else {
+          currentBuffer += value
+        }
+        if (!currentBuffer.includes('\n')) continue
+        const lines = currentBuffer.split('\n')
+        // Get remaining data and put it into the buffer
+        currentBuffer = lines.pop()
+
+        for (const line of lines) {
+          if (!line) {
+            container.append(JSON.parse(currentData).text)
+            currentData = ''
+          }
+          if (line.startsWith(DATA_LINE)) {
+            const data = line.slice(DATA_LINE.length).trimStart()
+            if (currentData) currentData += '\n'
+            currentData += data
+          }
+        }
       }
+
     })
   }
 
