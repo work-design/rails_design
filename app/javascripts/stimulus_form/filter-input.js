90baea74d1cb7694e058d3bb5e0fa1889b7cad56
qinmingyuan <mingyuan0715@foxmail.com>
2025-06-10 00:15:07 +0800

    d

--- app/javascripts/stimulus_form/filter-input.js
+++ app/javascripts/stimulus_form/filter-input.js
@@ -13,4 +13,16 @@ export default class extends BaseController {
     el.form.requestSubmit()
   }
 
+  confirm(e) {
+    const el = e.currentTarget
+    const lteEl = Array.from(el.form.elements).find(i => i.name === 'created_at-gte')
+    console.debug(el.value)
+    el.blur()
+
+
+    if (el.type === 'datetime-local' && el.name.endsWith('-lte') && lteEl.value) {
+      el.form.requestSubmit()
+    }
+  }
+
 }
