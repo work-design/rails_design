4227ad822e3e349629027fae181ad0d77b55312d
qinmingyuan <mingyuan0715@foxmail.com>
2025-06-09 21:39:08 +0800

    moved repeat column

--- app/javascripts/stimulus_form/filter-input.js
+++ app/javascripts/stimulus_form/filter-input.js
@@ -5,6 +5,11 @@ export default class extends BaseController {
 
   submit(e) {
     const el = e.currentTarget
+    for (const input of el.form.elements) {
+      if (input.name === el.name && input.type === 'hidden') {
+        input.remove()
+      }
+    }
     el.form.requestSubmit()
   }
 
