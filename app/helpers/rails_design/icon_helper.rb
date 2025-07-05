b5621c47bd24ce7e025a9675991ca5a25d080fdd
qinmingyuan <mingyuan0715@foxmail.com>
2025-07-05 15:45:29 +0800

    icon helpger

---
+++ app/helpers/rails_design/icon_helper.rb
@@ -0,0 +1,15 @@
+module RailsDesign::IconHelper
+
+  def svg_tag(name, **options)
+    content_tag :svg, options do
+      content_tag :use, nil, 'xlink:href' => "#{asset_path 'icons.svg'}##{name}"
+    end
+  end
+
+  def svg_light_tag(name, **options)
+    content_tag :svg, options do
+      content_tag :use, nil, 'xlink:href' => "#{asset_path 'icons_light.svg'}##{name}"
+    end
+  end
+
+end
