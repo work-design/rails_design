18f0e0449f542872b8cdffecdc035feada6d3582
qinmingyuan <mingyuan0715@foxmail.com>
2025-07-05 17:47:27 +0800

    beter hegper

--- app/helpers/rails_design/icon_helper.rb
+++ app/helpers/rails_design/icon_helper.rb
@@ -2,13 +2,13 @@ module RailsDesign::IconHelper
 
   def svg_tag(name, **options)
     content_tag :svg, options do
-      content_tag :use, nil, 'xlink:href' => "#{asset_path 'icons.svg'}##{name}"
+      content_tag :use, nil, 'href' => "#{asset_path 'icons.svg'}##{name}"
     end
   end
 
   def svg_light_tag(name, **options)
     content_tag :svg, options do
-      content_tag :use, nil, 'xlink:href' => "#{asset_path 'icons_light.svg'}##{name}"
+      content_tag :use, nil, 'href' => "#{asset_path 'icons_light.svg'}##{name}"
     end
   end
 
