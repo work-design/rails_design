9e81f58714e3214a2ef2c6721194aa74d2cdd98f
qinmingyuan <mingyuan0715@foxmail.com>
2025-07-07 12:02:00 +0800

    svg logc

--- app/helpers/rails_design/icon_helper.rb
+++ app/helpers/rails_design/icon_helper.rb
@@ -12,4 +12,10 @@ module RailsDesign::IconHelper
     end
   end
 
+  def svg_regular_tag(name, **options)
+    content_tag :svg, options do
+      content_tag :use, nil, 'href' => "#{asset_path 'icons_regular.svg', host: request.host_with_port}##{name}"
+    end
+  end
+
 end
