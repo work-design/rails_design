a311a3d9f81c1899ae6b7a31b29f548e27d65bcd
qinmingyuan <mingyuan0715@foxmail.com>
2025-07-31 16:29:30 +0800

    simple icon helper

--- app/helpers/rails_design/icon_helper.rb
+++ app/helpers/rails_design/icon_helper.rb
@@ -1,20 +1,8 @@
 module RailsDesign::IconHelper
 
-  def svg_tag(name, **options)
+  def svg_tag(name, kind: 'solid', **options)
     content_tag :svg, options do
-      content_tag :use, nil, 'href' => "#{asset_path 'icons.svg', host: request.host_with_port}##{name}"
-    end
-  end
-
-  def svg_light_tag(name, **options)
-    content_tag :svg, options do
-      content_tag :use, nil, 'href' => "#{asset_path 'icons_light.svg', host: request.host_with_port}##{name}"
-    end
-  end
-
-  def svg_regular_tag(name, **options)
-    content_tag :svg, options do
-      content_tag :use, nil, 'href' => "#{asset_path 'icons_regular.svg', host: request.host_with_port}##{name}"
+      content_tag :use, nil, 'href' => "#{asset_path "icons_#{kind}.svg", host: request.host_with_port}##{name}"
     end
   end
 
