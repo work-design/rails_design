bc3babe51464f32a76c260f43c7e883c3ef525a8
qinmingyuan <mingyuan0715@foxmail.com>
2025-08-19 18:19:27 +0800

    define

--- app/javascripts/rails_design/weixin_script.js
+++ app/javascripts/rails_design/weixin_script.js
@@ -1,51 +1,52 @@
 const weixin_fetch = function({ url = location.href, success, ...args } = {}) {
   if (typeof wx === 'undefined') {
-    return
+    console.debug('wx is undefined!')
+  } else {
+    fetch('/wechat/js', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({url: url})
+    }).then(response => {
+      return response.json()
+    }).then(body => {
+      let config = {
+        debug: body['debug'],
+        appId: body['appid'],
+        timestamp: body['timestamp'],
+        nonceStr: body['noncestr'],
+        signature: body['signature'],
+        jsApiList: body['apis'],
+        openTagList: body['open_tags']
+      }
+      if (body['beta']) {
+        Object.assign(config, {beta: true})
+      }
+      if (body['debug']) {
+        alert('body is:' + JSON.stringify(config))
+      }
+      wx.config(config)
+      wx.ready(() => {
+        if (body['debug']) {
+          alert('wx.config ready')
+        } else {
+          console.debug('ready, ok')
+        }
+        if (success) {
+          success(args)
+        }
+      })
+      wx.error(res => {
+        if (body['debug']) {
+          alert('wx.config: ' + JSON.stringify(res) + '\n' + `location: ${location.href}`)
+        } else {
+          console.debug('wx.config:', res)
+        }
+      })
+    })
   }
-  fetch('/wechat/js', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ url: url })
-  }).then(response => {
-    return response.json()
-  }).then(body => {
-    let config = {
-      debug: body['debug'],
-      appId: body['appid'],
-      timestamp: body['timestamp'],
-      nonceStr: body['noncestr'],
-      signature: body['signature'],
-      jsApiList: body['apis'],
-      openTagList: body['open_tags']
-    }
-    if (body['beta']) {
-      Object.assign(config, { beta: true })
-    }
-    if (body['debug']) {
-      alert('body is:' + JSON.stringify(config))
-    }
-    wx.config(config)
-    wx.ready(() => {
-      if (body['debug']) {
-        alert('wx.config ready')
-      } else {
-        console.debug('ready, ok')
-      }
-      if (success) {
-        success(args)
-      }
-    })
-    wx.error(res => {
-      if (body['debug']) {
-        alert('wx.config: ' + JSON.stringify(res) + '\n' + `location: ${location.href}`)
-      } else {
-        console.debug('wx.config:', res)
-      }
-    })
-  })
 }
 window.weixin_fetch = weixin_fetch
 
