7381b82530fda9fca8733e99eebbaef723709f49
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-16 13:53:14 +0800

    sep transition and animateion

--- app/javascripts/stimulus_weui/index.js
+++ app/javascripts/stimulus_weui/index.js
@@ -4,11 +4,5 @@ application.register('mask', MaskController)
 import WeuiActionsheetController from './weui-actionsheet'
 application.register('weui-actionsheet', WeuiActionsheetController)
 
-import WeuiDatepickerController from './weui-datepicker'
-application.register('weui-datepicker', WeuiDatepickerController)
-
 import WeuiDialogController from './weui-dialog'
 application.register('weui-dialog', WeuiDialogController)
-
-import WeuiFormController from './weui-form'
-application.register('weui-form', WeuiFormController)
