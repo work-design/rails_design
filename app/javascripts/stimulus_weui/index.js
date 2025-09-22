97d7c9235a8ecbae24f1931090852b68f637d6c1
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-22 15:56:16 +0800

    moved unused files

--- app/javascripts/stimulus_weui/index.js
+++
@@ -1,8 +0,0 @@
-import MaskController from './weui-mask'
-application.register('mask', MaskController)
-
-import WeuiActionsheetController from './weui-actionsheet'
-application.register('weui-actionsheet', WeuiActionsheetController)
-
-import WeuiDialogController from './weui-dialog'
-application.register('weui-dialog', WeuiDialogController)
